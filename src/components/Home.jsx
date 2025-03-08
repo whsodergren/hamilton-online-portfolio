a66b83388ecd411cb526a22d73a0830acc451d49
whsodergren <whsodergren@crimson.ua.edu>
2025-03-07 18:16:15 -0600

    text updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,9 @@ const Home = () => {
     <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
+                
                 <h2 className='text-4xl sm:text-7xl font-bold text-white font-serif'>Hello, I am <text className='text-blue-500'>Hamilton</text></h2>
-                <p className='text-gray-500 py-4'>I am a full-stack developer who is passionate about building efficient, scalable, and user-friendly applications that drive business success.</p>
+                <h3 className='text-3xl text-gray-300 py-4'>Full-Stack Web Developer</h3>
                 <div>
                     <Link to='portfolio' smooth duration={500} className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-700'>
                         Portfolio 
